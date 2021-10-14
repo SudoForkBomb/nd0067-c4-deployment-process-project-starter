f487dcc67aa971d40d7cac0e8d5dbcc4d7c74075
Chris Taylor <chrisrtaylor123@gmail.com>
2021-10-14 15:38:21 -0800

    fix: added aws id and secret to fix permissions

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,5 +1,5 @@
-import * as dotenv from "dotenv";
-dotenv.config();
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
@@ -7,12 +7,13 @@ export const config = {
   database: process.env.POSTGRES_DB,
   port: Number(process.env.PORT),
   host: process.env.POSTGRES_HOST,
-  dialect: "postgres",
+  dialect: 'postgres',
   aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
+  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
+  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
   },
-};
+}
